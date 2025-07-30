dafb434ef387b15c0f72e524bb6dd7ec9969521f
CarlosLacerda <carlosalacerda01@gmail.com>
2025-07-30 18:59:50 -0300

    Utilizando Expressões Lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,7 +16,9 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator <Product> comp = (p1, p2) ->  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());;
+		
+		list.sort(comp);
 		
 		for (Product p : list) {
 			System.out.println(p);
